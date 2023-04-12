bebb6f2fa8b951442d1b02820355efe10a7b0b3a
Neves888 <helen.vneves@gmail.com>
2023-04-12 08:31:48 -0300

    add req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file, "r") as file:
+                file = [word.replace("\n", "") for word in file.readlines()]
+                return file
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido")
