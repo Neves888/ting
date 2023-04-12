431bcc45f64e4ac5676e7581ae524f1750fc2f19
Neves888 <helen.vneves@gmail.com>
2023-04-12 13:32:12 -0300

    add req 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list = []
+    file_text_line = instance.search(0)["linhas_do_arquivo"]
+    data = {
+        "palavra": word,
+        "arquivo": instance.search(0)["nome_do_arquivo"],
+        "ocorrencias": [
+            {
+                "linha": i + 1
+            } for i, line in enumerate(file_text_line)
+            if word.lower() in line.lower()
+        ]
+    }
+
+    if data["ocorrencias"]:
+        list.append(data)
+
+    return list
 
 
 def search_by_word(word, instance):
