ba4b3d18e65062103c7aa2ba41c3d5b9d6fefff4
Neves888 <helen.vneves@gmail.com>
2023-04-12 09:10:53 -0300

    add req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in range(len(instance)):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            return None
+    file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+        }
+    file_out = sys.stdout
+    instance.enqueue(data)
+    print(data, file=file_out)
 
 
 def remove(instance):
